37d04d76fb74772acee59b1a9e77bdfce893e3dc
= <=>
2022-07-19 16:12:07 -0400

    wip

---
+++ js/userController.js
@@ -0,0 +1,34 @@
+let signupForm = document.getElementById("signupForm");
+let signupButton = document.getElementById("signupButton");
+
+
+  
+const addUserToDatabaseRequest = async (firstName, lastName, email, password) => {
+    let response = await fetch(url,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+            "firstName": firstName,
+            "lastName": lastName,
+            "email": email,
+            "password": password
+        }),
+            });
+    // if the response is bad
+    if(!response.ok){
+        throw new Error(`There is an error with status ${response.status}`)
+    }
+    let itemsJson = response.json();
+    return itemsJson;
+}
+
+const addUser = async (event) =>{
+    event.preventDefault();
+    url="http://localhost:8080/api/user/signup";
+    let user = await addUserToDatabaseRequest(signupForm.FirstName.value, signupForm.LastName.value, signupForm.email.value, signupForm.password.value);
+}
+
+signupButton.addEventListener('click',addUser);
