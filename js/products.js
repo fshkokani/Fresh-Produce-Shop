46e1e405b9a55a52375eefa78bc253641a1eabf6
= <=>
2022-07-20 13:01:13 -0400

    wip

--- js/products.js
+++ js/products.js
@@ -15,9 +15,10 @@ let listProductDiv = document.getElementById('list-products');
 // Function that add a new items to the list in HTML structure.
 // used split to mitigate fake path in local storage
 
-loadProductsFromLocalStorage().forEach(element => {
+if(loadProductsFromLocalStorage() != null){
+  loadProductsFromLocalStorage().forEach(element => {
     let newCardHTML = ` 
-  <div class=" col-sm-12 col-md-6 col-lg-4 card" style="width: 18rem;">
+  <div class=" col-sm-12 col-md-6 col-lg-4 card" style="width: 18rem;" id="${element.id}">
   <img src="/assets/${element.img.split('\\')[2]}" class="card-img-top" alt="...">
   <ul class="list-group list-group-flush">
     <li class="list-group-item">${element.name}</li>
@@ -26,11 +27,82 @@ loadProductsFromLocalStorage().forEach(element => {
   </ul>
   <div class="input-group mb-3">
     <input type="number" max="10" min="0" class="form-control" placeholder="Quantity" aria-label="Quantity" aria-describedby="button-addon1">
-    <button class="btn btn-warning" type="button" id="button-addon1">Add</button>
+    <button class=" btn btn-warning" type="button" id="button-addon1">Add</button>
 </div>
   </div>
   `;
   // add cards without removing the old cards
     listProductDiv.innerHTML += newCardHTML;
-    console.log(element.img.split('\\'))
-});
+})
+}
+;
+
+
+// Get All items
+const loadProductsFromDatabaseRequest = async () => {
+  let response = await fetch(url);
+  // if the response is bad
+  if(!response.ok){
+      throw new Error(`There is an error with status ${response.status}`)
+  }
+  let itemsJson = response.json();
+  return itemsJson;
+}
+
+
+const loadProductsFromDatabase = async () => {
+url = "http://localhost:8080/api/item";
+let items = await loadProductsFromDatabaseRequest();
+
+items.forEach(element => {
+
+  let newCardHTML = ` 
+  <div class=" col-sm-12 col-md-6 col-lg-4 card" style="width: 18rem;">
+  <img src="/assets/${element.imageUrl.split('\\')[2]}" class="card-img-top" alt="...">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${element.name}</li>
+    <li class="list-group-item">${element.description}:</li>
+    <li class="list-group-item">$${element.price} per pound</li>
+  </ul>
+  <div class="input-group mb-3">
+    <input type="number" max="10" min="0" class="form-control" placeholder="Quantity" aria-label="Quantity" aria-describedby="button-addon1">
+    <button class="addToCartButtons" class="btn btn-warning" type="button" id="${element.id}">Add</button>
+
+</div>
+  </div>
+  `;
+  // add cards without removing the old cards
+    listProductDiv.innerHTML += newCardHTML;
+
+  });
+  
+}
+
+let addToCartRequest = async () => {
+  let response = await fetch(url);
+  // if the response is bad
+  if(!response.ok){
+      throw new Error(`There is an error with status ${response.status}`)
+  }
+  let itemsJson = response.json();
+  return itemsJson;
+}
+const addToCart = async()=>{
+  console.log('hello')
+  url= "http://localhost:8080/api/item/cart";
+  let cartItems = await addToCartRequest();
+}
+
+window.addEventListener('load', loadProductsFromDatabase);
+//Event listener for the addToCart
+
+
+document.addEventListener('click', function(e)
+{
+   if(e.target.className == "addToCartButtons"){
+    console.log(e.target.className)
+    let param=e.target.id;
+    addToCart();
+   }
+    
+  });
--- js/userController.js
+++ js/userController.js
@@ -1,9 +1,12 @@
 let signupForm = document.getElementById("signupForm");
 let signupButton = document.getElementById("signupButton");
 
+let signinForm = document.getElementById("signinForm");
+let signinButton = document.getElementById("signinButton");
+
 
   
-const addUserToDatabaseRequest = async (firstName, lastName, email, password) => {
+const signUpRequest = async (firstName, lastName, email, password) => {
     let response = await fetch(url,
         {
             method: 'POST',
@@ -26,9 +29,31 @@ const addUserToDatabaseRequest = async (firstName, lastName, email, password) =>
     return usersJson;
 }
 
-const addUser = async () =>{
+const signUp = async (event) =>{
+    event.preventDefault();
     url="http://localhost:8080/api/user/signup";
-    let user = await addUserToDatabaseRequest(signupForm.FirstName.value, signupForm.LastName.value, signupForm.email.value, signupForm.password.value);
+    let user = await signUpRequest(signupForm.FirstName.value, signupForm.LastName.value, signupForm.email.value, signupForm.password.value);
 }
 
-signupButton.addEventListener('click',addUser);
+const signInRequest = async () => {
+    let response = await fetch(url);
+    // if the response is bad
+    if(!response.ok){
+        throw new Error(`There is an error with status ${response.status}`)
+    }
+    let usersJson = response.json();
+    console.log(usersJson)
+    return usersJson;
+}
+const signIn = async(event) =>{
+event.preventDefault();
+    url=`http://localhost:8080/api/user/signin?email=${signinForm.email.value}&password=${signinForm.password.value}`;
+    let user = await signInRequest();
+}
+
+if (signupButton != null){
+    signupButton.addEventListener('click',signUp);
+}
+if(signinButton !=null){
+    signinButton.addEventListener('click', signIn);
+}
